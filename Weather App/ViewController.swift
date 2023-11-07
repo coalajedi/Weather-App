38af9811fd48142038a33734e9cac7f9b88e46ec
Felipe Duarte <coalajedi.ios@gmail.com>
2023-11-06 22:54:06 -0300

    Initial Commit

---
+++ Weather App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Felipe Duarte Silva on 06/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
